91fd5aebc0f6e7a4cc8485a44640dcf1cbb7384e
Junku <64190196+yjk5591@users.noreply.github.com>
2020-06-18 02:27:59 +0900

    로그인 기능, 로그인 후 로그아웃 버튼으로 변경 완료

--- project-sw/public/account.js
+++ project-sw/public/account.js
@@ -1,17 +1,17 @@
 firebase.auth().onAuthStateChanged(function(user){
   if(user){
+    document.getElementById("off_btn").style.display="block"
     document.getElementById("on_btn").style.display="none"
-    document.getElementById("off_div").style.display="block"
   }
   else{
-    document.getElementById("on_btn").style.display="block"
     document.getElementById("off_btn").style.display="none"
+    document.getElementById("on_btn").style.display="block"
   }
 });
 
 function login(){
-  var userEmail=document.getElementById("email").value;
-  var userPass=document.getElementById("password").value;
+  var userEmail=document.getElementById("userEmail").value;
+  var userPass=document.getElementById("userPass").value;
 
   firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error){
     var errorCode=error.code;
@@ -19,24 +19,36 @@ function login(){
 
     window.alert("Error: "+errorMessage);
   });
+  window.alert("Sign in complete!");
+}
+
+function logout(){
+  firebase.auth().signOut();
 }
 
 function aut_signUp(){
+  var username=document.getElementById("username").value;
+  var email=document.getElementById("email").value;
   var password=document.getElementById("password").value;
   var retype_password=document.getElementById("retype_password").value;
+  var address=document.getElementById("address").value;
+  var phone_num=document.getElementById("phone_num").value;
+  var birthday=document.getElementById("birthday").value;
+  var hospital_name=document.getElementById("hospital_name").value;
+  var area=document.getElementById("area").value;
   if(password==retype_password){
-    alert("1");
-    var rootRef = firebase.database().ref('User/');
+    var rootRef = firebase.database().ref('User/Admin/');
     rootRef.push({
-      username:document.getElementById("username").value,
-      password:document.getElementById("password").value,
-      address:document.getElementById("address").value,
-      phone_num:document.getElementById("phone_num").value,
-      birthday:document.getElementById("birthday").value,
-      hospital_name:document.getElementById("hospital_name").value,
-      area:document.getElementById("area").value
+      username:username,
+      email:email,
+      password:password,
+      address:address,
+      phone_num:phone_num,
+      birthday:birthday,
+      hospital_name:hospital_name,
+      area:area
     });
-    window.alert("Welcome!");
+    window.alert("Welcome! Please login again");
   }
   else{
     window.alert("Password inconsistency detected");
@@ -45,19 +57,27 @@ function aut_signUp(){
 
 function mem_signUp(){
   var username=document.getElementById("username").value;
+  var email=document.getElementById("email").value;
   var password=document.getElementById("password").value;
   var retype_password=document.getElementById("retype_password").value;
   var address=document.getElementById("address").value;
   var phone_num=document.getElementById("phone_num").value;
   var birthday=document.getElementById("birthday").value;
   var number_pet=document.getElementById("number_pet").value;
-  var rootRef = firebase.database().ref('User/Member/');
-    rootRef.push({
-      username:username,
-      password:password,
-      address:address,
-      phone_num:phone_num,
-      birthday:birthday,
-      number_pet:number_pet
-    });
+  if(password==retype_password){
+    var rootRef = firebase.database().ref('User/Member/');
+      rootRef.push({
+        username:username,
+        email:email,
+        password:password,
+        address:address,
+        phone_num:phone_num,
+        birthday:birthday,
+        number_pet:number_pet
+      });
+      window.alert("Welcome! Please login again");
+  }
+  else{
+    window.alert("Password inconsistency detected");
+  }
 }
